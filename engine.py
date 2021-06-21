441a88475eb611dff1dd0561fee45efb3eba2f8e
James Clawson <nomclawson@byui.edu>
2021-06-21 15:13:14 -0600

    Monday's stuff James

--- engine.py
+++ engine.py
@@ -1,1 +1,50 @@
 # the Engine
+
+import cv2
+import numpy as np
+from random import randint
+from circle import Circle
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+
+class Engine:
+	def __init__(self):
+		self.objects = []
+
+		self.window_h_w_d = (SCREEN_HEIGHT,SCREEN_WIDTH,3)
+
+		# SAMPLE SIMULATIONS:
+		self.init_test_1()
+
+
+	def init_test_1(self):
+		# TEST circle
+		for i in range(1000):
+			c = Circle(randint(0,SCREEN_WIDTH),randint(0,SCREEN_HEIGHT),randint(5,20),(randint(0,255),randint(0,255),randint(0,255)))
+			c.velocity.dx = randint(-3,3)
+			c.velocity.dy = randint(-3,3)
+			self.objects.append(c)
+
+	def run(self):
+		for i in range(1000):
+			self.update()
+			self.draw()
+
+
+	def draw(self):
+		frame = np.zeros(self.window_h_w_d, dtype=np.uint8)
+		for obj in self.objects:
+			obj.draw(frame)
+		cv2.imshow("Physics Engine", frame)
+		cv2.waitKey(1)
+
+
+	def update(self):
+		for obj in self.objects:
+			obj.advance()
+
+eng = Engine()
+
+
+eng.run()
--- shape.py
+++ shape.py
@@ -3,7 +3,7 @@ from point import Point
 from velocity import Velocity
 
 class Shape:
-	def __init__(self,x=0,y=0):
+	def __init__(self, x=0, y=0, color=(255,255,255)):
 		self.center = Point(x,y)
 		self.velocity = Velocity(0,0)
 		self.angle = 0 # orientation (NOT direction)
@@ -12,18 +12,25 @@ class Shape:
 		
 		self.mass = .2 # measured in kg (for now at least)
 		self.infinite_mass = False
+		# (b,g,r)
+		self.color = color
 
 
 	def draw(self):
 		print("Draw needs to be implemented!")
 
 	def check_collision(self):
-		print("check_collision needs to be implemented!")
+		# print("check_collision needs to be implemented!")
+		pass
 
 	def on_collision(self):
-		print("on_collision needs to be implemented!")
+		# print("on_collision needs to be implemented!")
+		pass
 
 	def advance(self):
+		if self.check_collision():
+			self.on_collision()
+
 		self.center.x += self.velocity.dx
 		self.center.y += self.velocity.dy
 
