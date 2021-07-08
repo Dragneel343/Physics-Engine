46a1f788e3f1d9c68fa2513e68adabca5b7eff65
Nathan Robison <ncrobiso@yahoo.com>
2021-07-07 20:42:23 -0600

    Finished circle on circle collision. Not as smooth as I would like, but it works.

--- circle.py
+++ circle.py
@@ -1,23 +1,25 @@
 # Creat circle objects
 # Hey Guys
 from shape import Shape
+from random import randint
 import cv2
 
 class Circle(Shape):
-	def __init__(self, x=0, y=0, radius=5, color=(255,255,255)):
-		super().__init__(x=x, y=y, color=color)
-		self.radius = radius
+    def __init__(self, x=0, y=0, radius=5, color=(255,255,255)):
+        super().__init__(x=x, y=y, color=color)
+        self.radius = radius
 
-	def draw(self, frame):
-		
-		frame = cv2.circle(frame, (self.center.x, self.center.y), self.radius, self.color, -1)
-		return frame
+    def draw(self, frame):
+        
+        frame = cv2.circle(frame, (self.center.x, self.center.y), self.radius, self.color, -1)
+        return frame
 
-	def on_collision(self):
-		# TODO
-		# Should adjust speed (magnitude of velocity) by a factor of .9 (loses some speed to simulate inelastic collision)
-		pass
+    def on_collision(self, velocity):
+        # TODO
+        # Should adjust speed (magnitude of velocity) by a factor of .9 (loses some speed to simulate inelastic collision)
 
-	def advance(self):
-		super().advance()
-		self.center += self.velocity  
+        pass
+
+    def advance(self):
+        super().advance()
+        self.center += self.velocity  
--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+import math
 from random import randint
 from circle import Circle
 from polygon import Polygon
@@ -11,99 +12,134 @@ from globals import *
 
 
 class Engine:
-	def __init__(self):
-		self.objects = []
+    def __init__(self):
+        self.objects = []
 
-		self.window_shape = (SCREEN_HEIGHT,SCREEN_WIDTH,3)
+        self.window_shape = (SCREEN_HEIGHT,SCREEN_WIDTH,3)
 
-		self.collision_dict = {
-			(Polygon, Polygon) : self.poly_poly_check,
-			(Polygon, Circle) : self.poly_circ_check,
-			(Circle, Polygon) : self.poly_circ_check,
-			(Rectangle, Polygon) : self.poly_poly_check,
-			(Polygon, Rectangle) : self.poly_poly_check,
-			(Rectangle, Circle) : self.poly_circ_check,
-			(Circle, Rectangle) : self.poly_circ_check,
-			(Circle, Circle) : self.circ_circ_check
-		}
+        self.collision_dict = {
+            (Polygon, Polygon) : self.poly_poly_check,
+            (Polygon, Circle) : self.poly_circ_check,
+            (Circle, Polygon) : self.poly_circ_check,
+            (Rectangle, Polygon) : self.poly_poly_check,
+            (Polygon, Rectangle) : self.poly_poly_check,
+            (Rectangle, Circle) : self.poly_circ_check,
+            (Circle, Rectangle) : self.poly_circ_check,
+            (Circle, Circle) : self.circ_circ_check
+        }
 
-		# SAMPLE SIMULATIONS:
-		self.init_test_1()
-
-
-
-	def init_test_1(self):
-		# TEST circle
-		pts = (Point(100,100),Point(140,100),Point(140,140),Point(100,140))
-		poly = Polygon(pts,color=(255,100,150))
-		poly.velocity.dx = 2
-		poly.velocity.dy = -2
-		self.objects.append(poly)
-
-		rect = Rectangle(300, 400, 20, 20,(255,255,255))
-		poly.velocity.dx = -.5
-		poly.velocity.dy = 7
-		self.objects.append(rect)
+        # SAMPLE SIMULATIONS:
+        self.init_test_1()
 
 
-		for i in range(100):
-			c = Circle(randint(0,SCREEN_WIDTH),randint(0,SCREEN_HEIGHT),randint(15,40),(randint(0,255),randint(0,255),randint(0,255)))
-			c.velocity.dx = randint(-3,3)
-			c.velocity.dy = randint(-1,3)
-			self.objects.append(c)
 
-	def run(self):
-		for i in range(500):
-			self.update()
-			self.draw()
+    def init_test_1(self):
+        # TEST circle
+        pts = (Point(100,100),Point(140,100),Point(140,140),Point(100,140))
+        poly = Polygon(pts,color=(255,100,150))
+        poly.velocity.dx = 2
+        poly.velocity.dy = -2
+        self.objects.append(poly)
 
-
-	def draw(self):
-		# make a new black frame
-		frame = np.zeros(self.window_shape, dtype=np.uint8)
-
-		# draw each object
-		for obj in self.objects:
-			obj.draw(frame)
-		cv2.imshow("Physics Engine", frame)
-		cv2.waitKey(1)
+        rect = Rectangle(300, 400, 20, 20,(255,255,255))
+        poly.velocity.dx = -.5
+        poly.velocity.dy = 7
+        self.objects.append(rect)
 
 
-	def update(self):
-		# Update each object one frame
-		for obj in self.objects:
-			obj.advance()
+        for i in range(40):
+            c = Circle(randint(0,SCREEN_WIDTH),randint(0,SCREEN_HEIGHT),randint(15,40),(randint(0,255),randint(0,255),randint(0,255)))
+            c.velocity.dx = randint(-3,3)
+            c.velocity.dy = randint(-3,3)
+            self.objects.append(c)
 
-		self.check_collisions()
-
-	def check_collisions(self):
-		# Non-optimized collision checking - O(n^2) performance
-		# for obj1 in self.objects:
-		# 	for obj2 in self.objects:
-		# 		if obj1 == obj2:
-		# 			break
-		# 		self.collision_dict[(type(obj1),type(obj2))](obj1,obj2)
-		pass
+    def run(self):
+        for i in range(500):
+            self.check_collisions()
+            self.update()
+            self.draw()
+        cv2.destroyAllWindows()
 
 
-	def poly_poly_check(self, poly1, poly2):
-		# Collision check between two polygons
-		pass
+    def draw(self):
+        # make a new black frame
+        frame = np.zeros(self.window_shape, dtype=np.uint8)
 
-	def poly_circ_check(self, poly, circ):
-		# Collision check between circle and polygon
+        # draw each object
+        for obj in self.objects:
+            obj.draw(frame)
+        cv2.imshow("Physics Engine", frame)
+        cv2.waitKey(1)
 
-		# corrects variable name to type associated
-		if type(poly) != Polygon:
-			poly, circ = circ, poly
-		pass
 
-	def circ_circ_check(self, circ1, circ2):
-		# Collision check between two circles
+    def update(self):
+        # Update each object one frame
+        for obj in self.objects:
+            obj.advance()
 
-		# !!!___TODO___!!! 
-		# This should be the easiest to implement and could be implemented immediately
-		pass
+        self.check_collisions()
+
+    def check_collisions(self):
+        # Non-optimized collision checking - O(n^2) performance
+        tested = set(())
+        for obj1 in self.objects:
+             for obj2 in self.objects:
+                 tested.add((obj2, obj1))
+                 #frozen = frozenset(pair)
+                 if (obj1, obj2) in tested:
+                     break
+                 if obj1 == obj2:
+                     break
+                 self.collision_dict[(type(obj1),type(obj2))](obj1,obj2)
+                 #tested.add(frozen)
+        pass
+
+
+    def poly_poly_check(self, poly1, poly2):
+        # Collision check between two polygons
+        pass
+
+    def poly_circ_check(self, poly, circ):
+        # Collision check between circle and polygon
+
+        # corrects variable name to type associated
+        if type(poly) != Polygon:
+            poly, circ = circ, poly
+        pass
+
+    def circ_circ_check(self, circ1, circ2):
+        
+        #Checks bounding boxes around circles. 
+        #Formula taken from https://gamedevelopment.tutsplus.com/tutorials/when-worlds-collide-simulating-circle-circle-collisions--gamedev-769
+        if (circ1.center.x + circ1.radius + circ2.center.x > circ2.center.x
+            and circ1.center.x < circ2.center.x + circ1.radius + circ2.radius
+            and circ1.center.y + circ1.center.y + circ2.radius > circ2.center.y
+            and circ1.center.y < circ2.center.y + circ1.radius + circ2.radius):
+        
+            # This formula is also the same as the article, but we came to it on our own. 
+            # Collision check between two circles
+            x = circ1.center.x - circ2.center.x
+            y = circ1.center.y - circ2.center.y
+            distance = math.sqrt((x**2 + y**2))
+            
+            #If circles are colliding
+            if (distance <= circ1.radius + circ2.radius):
+                newVelocity1x = (circ1.velocity.dx * (circ1.radius - circ2.radius) + (2 * circ2.radius * circ2.velocity.dx)) / (circ1.radius + circ2.radius)
+                newVelocity2x = (circ2.velocity.dx * (circ2.radius - circ1.radius) + (2 * circ1.radius * circ1.velocity.dx)) / (circ2.radius + circ1.radius)
+                newVelocity1y = (circ1.velocity.dy * (circ1.radius - circ2.radius) + (2 * circ2.radius * circ2.velocity.dy)) / (circ1.radius + circ2.radius)
+                newVelocity2y = (circ2.velocity.dy * (circ2.radius - circ1.radius) + (2 * circ1.radius * circ1.velocity.dx)) / (circ2.radius + circ1.radius)
+
+                circ1.velocity.dx = newVelocity1x 
+                circ1.velocity.dy = newVelocity1y
+                circ2.velocity.dx = newVelocity2x
+                circ2.velocity.dy = newVelocity2y
+            
+                circ1.advance()
+                circ2.advance()
+        
+        # !!!___TODO___!!! 
+        # This should be the easiest to implement and could be implemented immediately
+        pass
 
 
 
